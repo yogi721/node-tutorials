02b27281f724b501dbccc1933c02946cb15fb878
yogi721 <semlali.mehdi.1980@gmail.com>
2019-01-06 18:28:42 +0100

    modified 18-json/app.js file

--- node/18-json/app.js
+++ node/18-json/app.js
@@ -5,9 +5,13 @@ var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url);
     
     res.writeHead(200, {'Content-Type': 'application/json'});
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    var myObj = {
+        name: 'yogi721',
+        job: 'senior web dev',
+        age: 99
+    };
 
-    myReadStream.pipe(res);
+    res.end(JSON.stringify(myObj));
 });
 
 server.listen(3000, '127.0.0.1');
